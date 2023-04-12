d194556456bd4419849eeee742c27837257bce92
Chris Gibson <chrischarlesgibson@gmail.com>
2023-04-12 16:12:53 -0500

    added mui footer and login pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SearchComponent from "./pages/SearchComponent";
+import LoginComponent from "./pages/LoginComponent";
+import Footer from "./components/Footer";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename={process.env.PUBLIC_URL}>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<LoginComponent />} />
+          <Route exact path="/Search" element={<SearchComponent />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
   );
 }
 
